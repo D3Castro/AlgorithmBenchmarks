4705e743ba782b9fd7b8e605885cee7a147310c8
Daniel Castro <danbball@sbcglobal.net>
2018-03-14 15:23:06 -0700

    Finished basic program, file comments and readme required.

---
+++ Project1/Project1/Array.cpp
@@ -0,0 +1,230 @@
+#include "stdafx.h"
+#include "Array.h"
+
+using namespace std;
+
+Array::Array() {} //Default Constructor for the class
+
+Array::Array(int value) //Copy Constructor with size
+{
+	sizeOfArry = value; //User Pass size
+	arrData = new int[sizeOfArry];
+	for (int i = 0; i<sizeOfArry; i++)
+		arrData[i] = i;
+}
+
+Array::~Array() { } // Destructor for the class
+
+int Array::getSize()
+{
+	return sizeOfArry;
+}
+
+void Array::displayArray()
+{
+	cout << "[";
+	for (int i = 0; i<sizeOfArry; i++)
+		cout << arrData[i] << ", ";
+	cout << "]";
+}
+
+void Array::addToTop(int value)
+{
+	sizeOfArry = sizeOfArry + 1;
+	// with allocate memory
+	arrData =
+		(int*)realloc(arrData, sizeof(int)*sizeOfArry);
+	for (int i = sizeOfArry - 1; i>0; i--)
+		arrData[i] = arrData[i - 1];
+	arrData[0] = value;
+}
+
+void Array::addToEnd(int value)
+{
+	sizeOfArry = sizeOfArry + 1;
+	// with allocate memory
+	arrData =
+		(int*)realloc(arrData, sizeof(int)*sizeOfArry);
+	arrData[sizeOfArry - 1] = value;
+}
+
+void Array::removeFromTop()
+{
+	for (int i = 0; i<sizeOfArry; i++)
+		arrData[i] = arrData[i + 1];
+	arrData[sizeOfArry - 1] = NULL;
+}
+
+void Array::removeFromBottom()
+{
+	arrData[sizeOfArry - 1] = NULL;
+}
+
+void Array::reverseArray()
+{
+	int temp;
+	for (int i = 0; i<sizeOfArry / 2; i++)
+	{
+		temp = arrData[i];
+		arrData[i] = arrData[sizeOfArry - i - 1];
+		arrData[sizeOfArry - i - 1] = temp;
+	}
+}
+
+void Array::swapElements(int x, int y) 
+{
+	int temp;
+	temp = arrData[x];
+	arrData[x] = arrData[y];
+	arrData[y] = temp;
+}
+
+void Array::sortedFill()
+{
+	for (int i = 0; i<sizeOfArry; i++)
+		arrData[i] = i;
+}
+
+void Array::randFill()
+{
+	int i = 0;
+	while (i < sizeOfArry) {
+		arrData[i] = rand() % 100;
+		i++;
+	}
+}
+
+//May be incorrect implimentation 
+void Array::partialRFill()
+{
+	int swapI, temp;
+	int shuffleR = int(sizeOfArry * .1);
+	while (shuffleR >= 0) {
+		swapI = rand() % sizeOfArry;
+		temp = arrData[shuffleR];
+		arrData[shuffleR] = arrData[swapI];
+		arrData[swapI] = temp;
+		shuffleR--;
+	}
+
+}
+
+void Array::BubbleSort()
+{
+	for (int i = 0; i < sizeOfArry - 1; i++) {
+		for (int j = 0; j < sizeOfArry - i - 1; j++) {
+			if (arrData[j] > arrData[j + 1])
+				this->swapElements(j, j + 1);
+		}
+	}
+}
+
+void Array::InsertionSort()
+{
+	int j;
+
+	for (int i = 0; i < sizeOfArry; i++) {
+		j = i;
+
+		while (j > 0 && arrData[j] < arrData[j - 1]) {
+			this->swapElements(j, j - 1);
+			j--;
+		}
+	}
+}
+
+void Array::SelectionSort()
+{
+	int minIndex;
+
+	for (int i = 0; i < sizeOfArry - 1; i++) {
+		minIndex = i;
+
+		for (int j = i + 1; j < sizeOfArry; j++) {
+			if (arrData[j] < arrData[minIndex])
+				minIndex = j;
+		}
+
+		this->swapElements(minIndex, i);
+	}
+
+}
+
+void Array::MergeSort(int low, int high)
+{
+	int mid;
+
+	if (low < high) {
+		mid = (low + high) / 2;
+
+		//Split the data
+		MergeSort(low, mid);
+		MergeSort(mid + 1, high);
+
+		//Merge data to sort
+		Merge(low, mid, high);
+	}
+}
+
+void Array::Merge(int low, int mid, int high)
+{
+	int *temp = new int[high - low + 1];
+	int i = low;
+	int k = 0;
+	int j = mid + 1;
+
+	//Sort while merging
+	while (i <= mid && j <= high) {
+		if (arrData[i] < arrData[j]) {
+			temp[k] = arrData[i];
+			k++; i++;
+		}
+		else {
+			temp[k] = arrData[j];
+			k++; j++;
+		}
+	}
+
+	//Ensure all values were input
+	while (i <= mid) {
+		temp[k] = arrData[i];
+		k++; i++;
+	}
+
+	while (j <= high) {
+		temp[k] = arrData[j];
+		k++; j++;
+	}
+
+	//Put sorted values back into array A
+	for (i = low; i <= high; i++) {
+		arrData[i] = temp[i - low];
+	}
+}
+
+void Array::QuickSort(int low, int high)
+{
+	if (low < high) {
+		int partitionIndex = partition(low, high);
+
+		QuickSort(low, partitionIndex - 1);
+		QuickSort(partitionIndex + 1, high);
+	}
+}
+
+int Array::partition(int low, int high)
+{
+	int pivot = arrData[high];
+	int i = low - 1;
+
+	for (int j = low; j <= high - 1; j++) {
+		if (arrData[j] <= pivot) {
+			i++;
+			this->swapElements(i, j);
+		}
+	}
+
+	this->swapElements(i + 1, high);
+
+	return i + 1;
+}
---
+++ Project1/Project1/Array.h
@@ -0,0 +1,43 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+class Array
+{
+public:
+	Array(); //Default Constructor for the class
+	Array(int); //Copy Constructor with size
+	~Array(); //Destructor for the class
+
+	int getSize(); //Function to access the size
+	void displayArray(); //Display the content of the array
+	void addToTop(int); //Function to Add an element at the beginning
+	void addToEnd(int); //Function to Add an element at the end
+	void removeFromTop(); //Function to Remove an element at the beginning
+	void removeFromBottom(); //Function to Remove an element at the end
+	void reverseArray(); //Function to Inverse the order of the elements in the array
+	void swapElements(int, int); //Swap two elements in the array
+
+	void sortedFill(); //Fill the array with sorted values
+	void randFill(); //Fill the array with random values
+	void partialRFill(); //Fill the array partially shuffled 10%
+
+	void BubbleSort();
+
+	void InsertionSort();
+
+	void SelectionSort();
+
+	void MergeSort(int low, int high);
+
+	void Merge(int low, int mid, int high);
+
+	void QuickSort(int low, int high);
+
+	int partition(int low, int high);
+
+	int sizeOfArry; //Array size
+	int *arrData; //integer Array
+
+protected:
+private:
+};
+#endif // ARRAY_H
